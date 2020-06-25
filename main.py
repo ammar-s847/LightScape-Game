ef20ce4433a6107b8d019a9a4b7811b9876fe15d
Ammar <ammar.siddiqui847@gmail.com>
2020-06-25 13:22:48 -0400

    added colour variables

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 pygame.init()
 
-WinX, WinY = 500, 400
-window = pygame.display.set_mode((WinX, WinY)) #create 600x600 pxl window
+WinX, WinY = 360, 360
+window = pygame.display.set_mode((WinX, WinY)) # Creates Window
 pygame.display.set_caption("LightGame")
 run = True
 clock = pygame.time.Clock()
+
+# Colours
+YELLOW = (255, 255, 1)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # PHOTON OBJECT
 class photon(object):
@@ -13,37 +18,36 @@ class photon(object):
         self.x = x
         self.y = y
         self.radius = 10
-        self.color = (255, 255, 1)  # yellow
+        self.color = YELLOW
         self.vel = 20
 
-    def draw(self,window):
-        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius,
-                           0)  # I believe 0 means the circle will be completely filled
+    def draw(self, window):
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius, 0)
 
 def renderScreen():
-    window.fill((0,0,0))
+    window.fill(BLACK)
     light.draw(window)
     pygame.display.update()
 
 
-light = photon(10,10)
+light = photon(10, 10)
 keyReleased = True
 while run:
     clock.tick(60)
-    for event in pygame.event.get(): #ENDS RUN LOOP & CLOSES WINDOW WHEN RED X IS PRESSED
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             run= False
 
-    keys = pygame.key.get_pressed()  # this is a list
+    keys = pygame.key.get_pressed() # <- This is a List
     if keyReleased:
-        if keys[pygame.K_LEFT] == 1:  # if left key is pressed - and is to prevent square moving off screen
+        if keys[pygame.K_LEFT] == 1:
             if light.x >= light.radius * 2:
                 light.x -= light.vel
             else:
                 pass
             keyReleased = False
-        elif keys[pygame.K_RIGHT] == 1:  # x<screenWidth-width(of character)
+        elif keys[pygame.K_RIGHT] == 1:
             if light.x <= WinX - 20:
                 light.x += light.vel
             else:

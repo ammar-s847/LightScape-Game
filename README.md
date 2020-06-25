757154fa196fdac2dc06fb3e993aa5c627ec6669
Ammar <ammar.siddiqui847@gmail.com>
2020-06-25 11:03:00 -0400

    updated README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A Python game built with PyGame where you control a Photon along a grid and navi
 
 ### Collaborators
 * <a href="https://github.com/ammar-s847">ammar-s847</a>
+* <a href="https://github.com/ibbenj">ibbenj</a>
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import pygame
 import random
+
+# setup display
+pygame.init()
+WIDTH, HEIGHT = 800, 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("test2")
+
+# fonts
+
+
+# colours
+
+
+
+
+# grid generation
+startx = 200
+starty = 50
+space_interval = 25

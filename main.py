78e973483804aae0d2b305b3c6cc59b2a9806621
Ammar <ammar.siddiqui847@gmail.com>
2020-06-25 19:34:11 -0400

    added Timer Bar UI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
 
-WinX, WinY = 360, 360
+WinX, WinY = 360, 380
 window = pygame.display.set_mode((WinX, WinY)) #create 600x600 pxl window
 pygame.display.set_caption("LightScape")
 run = True
@@ -22,25 +22,15 @@ class Game(object):
             for row in self._board:
                 self._board[x] = file.readline().rstrip().split(' ')
                 x += 1
+        
+    def display(self):
+        global blackHoleList, light
         for row in range (0, 18):
             for col in range (0, 18):
-                if self._board[row][col] == 0: # blackHole, NOT ALLOWED
-                    self._board[row][col] = blackHole(row*20, col*20)
-                if self._board[row][col] == 1: # EMPTY, SET AS None
-                    self._board[row][col] = None
-                if self._board[row][col] == 2:  # Photon
-                    self._board[row][col] = photon(row*20, col*20)
-                #if(self._board[row][col] == 4):
-                    #WARP HOLE 1 OBJECT
-               # if(self._board[row][col] == 5):
-                    #WARP HOLE 2 OBJECT
-
-    def update_photon(self, screen_x, screen_y):
-        ''' @:param screen_x : screen x coordinate to be converted to the coordinates on the 18 by 18 array
-            @:param screen_y : screen y coordinate to be converted to the coordinates on the 18 by 18 array
-            :returns void, updates the photon's location on the board matrix directly
-            '''
-    # def check_for_game_win(self): work in progress
+                if self._board[row][col] == '0':
+                    blackHoleList.append(blackHole(col * 20, row * 20))
+                if self._board[row][col] == '2':
+                    light = photon(10 + (col * 20), 10 + (row * 20))
 
 # PHOTON OBJECT
 class photon(object):
@@ -52,8 +42,7 @@ class photon(object):
         self.vel = 20
 
     def draw(self, window):
-        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius,
-                           0)  # I believe 0 means the circle will be completely filled
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius, 0)
 
 class blackHole(object):
     def __init__(self, x, y):
@@ -75,52 +64,48 @@ class wormHole(object):
     def draw(self,window):
         window.blit(pygame.image.load('images/Wormhole.png'), (self.x, self.y))
 
+class finishLine(object):
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def draw(self,window):
+        window.blit(pygame.image.load('images/Finish.png'),(self.x,self.y))
 
 def renderScreen():
+    global window, goal, light
     window.fill(BLACK)
+    goal.draw(window)
     for i in range(0, len(blackHoleList)):
         blackHoleList[i].draw(window)
         # wormHoleList[i].draw(window)
     light.draw(window)  # render light after rendering wormholes
+
+    pygame.draw.rect(window, (211, 211, 211), (0, 360, 360, 20))
     timerText = font1.render('Time Left: ' + str(round(timeLeft)), 1, (255, 0, 0))
-    window.blit(timerText, (200, 340))
+    window.blit(timerText, (265, 365))
+
+    levelText = font1.render('Level: ' + str(level), 1, (255, 0, 0))
+    window.blit(levelText, (5, 365))
+
     pygame.display.update()
 
-# light = photon(10, 10)
+light = None
 blackHoleList = []
 wormHoleList = []
 game = Game('levels/level1.txt')
 for i in game._board:
     print(str(i) + "\n")
 
-for row in range (0, 18):
-    for col in range (0, 18):
-        if game._board[row][col] == '0':
-            blackHoleList.append(blackHole(col * 20, row * 20))
-        if game._board[row][col] == '2':
-            light = photon(10 + (col * 20), 10 + (row * 20))
-        #if(self._board[row][col] == 4):
-            #WARP HOLE 1 OBJECT
-        # if(self._board[row][col] == 5):
-            #WARP HOLE 2 OBJECT
-        #if col == '0': # blackHole, NOT ALLOWED
-            #blackHoleList.append(blackHole((i.index(n)) * 20, game._board.index(i) * 20))
-            #print("spawned BlackHole at " + str(i.index(n) * 20))
-        #if n == 2:  # Photon
-            #light = photon(n*20, n*20)
-        #if(self._board[row][col] == 4):
-            #WARP HOLE 1 OBJECT
-        # if(self._board[row][col] == 5):
-            #WARP HOLE 2 OBJECT
+game.display()
+# wormHoleList.append(wormHole((i+1)*60, (i+1)*80, i, 0))
 
-'''
-for i in range(0,2): # remove later - just for now to load in blackholes and worm holes
-    blackHoleList.append(blackHole((i+1)*40, (i+1)*40))
-    wormHoleList.append(wormHole((i+1)*60, (i+1)*80, i, 0)) '''
+goal = finishLine(300,100)
 
 timeLeft = 20
 lastTime = pygame.time.get_ticks()/1000
 font1 = pygame.font.SysFont('Comic Sans', 20)
+level = 1
 
 keyReleased = True
 while run:
@@ -139,9 +124,13 @@ while run:
         if wormHoleList[i].direction == 0:
             if light.x > wormHoleList[i].x and light.x < wormHoleList[i].x + 20:
                 if light.y > wormHoleList[i].y and light.y < wormHoleList[i].y + 20:
-                    print("teleport!")
                     light.x = 10 + wormHoleList[i + 1].x  # currently the wormhole pair must have adjacent indexcies - maybe we can make it such that it checks the index of the wormhole with the same wormhole.pair number & uses that wormhole's location
                     light.y = 10 + wormHoleList[i + 1].y  # teleports photon to other wormhole
+
+    #when goal is reached
+    if light.x > goal.x and light.x < goal.x + 20:
+        if light.y > goal.y and light.y < goal.y + 20:
+            print("goal reached - congrats!")
 
     keys = pygame.key.get_pressed()  # this is a list
     if keyReleased:
@@ -179,9 +168,7 @@ while run:
                 for i in range(0, len(blackHoleList)):
                     if light.y-light.radius > (blackHoleList[i].y - 20) and light.y-light.radius < (
                         blackHoleList[i].y + 40):
-                        print("y")
                         if light.x > blackHoleList[i].x and light.x < blackHoleList[i].x + 20:
-                            print("x")
                             bottomBarrier = True
                 if not bottomBarrier:
                     light.y -= light.vel
@@ -190,14 +177,12 @@ while run:
                 pass
             keyReleased = False
         elif keys[pygame.K_DOWN] == 1:
-            if light.y <= WinY - 20:
+            if light.y <= (WinY-20) - 20:
                 topBarrier = False
                 for i in range(0, len(blackHoleList)):
                     if light.y + light.radius > (blackHoleList[i].y - 20) and light.y + light.radius < (
                             blackHoleList[i].y + 20):
-                        print("y")
                         if light.x > blackHoleList[i].x and light.x < blackHoleList[i].x + 20:
-                            print("x")
                             topBarrier = True
                 if not topBarrier:
                     light.y += light.vel
